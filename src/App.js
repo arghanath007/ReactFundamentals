6d013420ba47c0d695c07f35138aa67e8732ed7b
Argha Nath <54589605+arghanath007@users.noreply.github.com>
2021-10-04 11:33:49 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import "./App.css";
+import { CardList } from "./Component/Card-list/card-list.component";
+
+import { Component } from "react";
+import { SearchBox } from "./Component/searchBox/search-box";
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      monsters: [],
+      searchField: "",
+    };
+  }
+
+  componentDidMount() {
+    fetch("http://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => this.setState({ monsters: users }));
+  }
+
+  handleChange = (e) => {
+    this.setState({ searchField: e.target.value });
+  };
+
+  render() {
+    const { monsters, searchField } = this.state;
+
+    const filteredMonsters = monsters.filter((monster) =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
+    return (
+      <div className="App">
+        <h1>Monsters List</h1>
+        <SearchBox
+          placeholder="Search Monsters"
+          handleChange={this.handleChange}
+        />
+        <CardList monsters={filteredMonsters} />
+      </div>
+    );
+  }
+}
+
+export default App;
